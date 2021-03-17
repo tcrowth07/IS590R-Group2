26d56c0ed1d348fb612f9245fa75129add7f0392
Tyler Crowther <tcrowth07@TyBook.local>
2021-03-17 17:01:29 -0600

    finished controller layer tests

---
+++ src/test/java/com/example/demo/api/JournalEntryControllerTest.java
@@ -0,0 +1,86 @@
+package com.example.demo.api;
+
+import com.example.demo.model.JournalEntry;
+import com.example.demo.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//import org.springframework.security.test.context.support.WithUserDetails;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@WithUserDetails("test")
+class JournalEntryControllerTest {
+
+    private WebApplicationContext context;
+    private MockMvc mvc;
+
+    @Autowired
+    public JournalEntryControllerTest(WebApplicationContext context) {
+        this.mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                //.apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    void addJournalEntry() throws Exception {
+        mvc.perform( MockMvcRequestBuilders
+                .post("/api/v1/journalentry")
+                .content(asJsonString(new JournalEntry(null, "test", "test markdown", "<p>test</p>", UUID.fromString("43ae77fc-d738-47c1-9abf-57c488fc277d"))))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    @Test
+    void getAllJournalEntries() throws Exception {
+        mvc.perform( MockMvcRequestBuilders
+                .get("/api/v1/journalentry")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    @Test
+    void getAllJournalEntriesByUserId() throws Exception {
+        mvc.perform( MockMvcRequestBuilders
+                .get("/api/v1/journalentry/user/{id}", "43ae77fc-d738-47c1-9abf-57c488fc277d")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    @Test
+    void getJournalEntryById() throws Exception {
+        mvc.perform( MockMvcRequestBuilders
+                .get("/api/v1/journalentry/{id}", "a2fdc1fb-fbe6-4bc4-864c-be43025e234e")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    @Test
+    void deleteJournalEntryById() throws Exception {
+        mvc.perform( MockMvcRequestBuilders
+                .delete("/api/v1/journalentry/{id}", "a2fdc1fb-fbe6-4bc4-864c-be43025e234e")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -5,9 +5,15 @@ import com.example.demo.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+
+
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class UserServiceTest {
@@ -19,11 +25,11 @@ public class UserServiceTest {
     void addUser() {
         UserService userService = new UserService(userDao);
         User testUser = new User(UUID.randomUUID(), "test", "test@gmail.com");
-//        Mockito.when(userDao.insertUser(testUser.getId(), testUser));
-//        Assertions.assertEquals("string", userService.addUser(testUser));
-//        Assertions.assertEquals(2, 2);
-        System.out.println(userService.addUser((testUser)));
-        Assertions.assertEquals(userService.addUser(testUser), "User " + testUser.getName() + " was added.");
+        Mockito.when(userDao.insertUser(testUser))
+                .thenReturn("User " + testUser.getName() + " was added");
+
+        String insertUserResponse = userService.addUser(testUser);
+        Assertions.assertEquals("User " + testUser.getName() + " was added", insertUserResponse);
     }
 
     @Test
@@ -32,7 +38,15 @@ public class UserServiceTest {
     }
 
     @Test
-    void getUserById() {
+    @DisplayName("Test should pass when a User with given id is returned")
+    void shouldGetUserById() {
+        UserService userService = new UserService(userDao);
+        User expectedUserResponse = new User(UUID.fromString("5e4259af-00eb-486e-82aa-8921ed0f1f6d"), "joe", "joe@gmail.com");
+        Mockito.when(userService.getUserById(UUID.fromString("5e4259af-00eb-486e-82aa-8921ed0f1f6d")))
+                .thenReturn(Optional.of(expectedUserResponse));
+
+        Optional<User> actualUserResponse = userService.getUserById(UUID.fromString("5e4259af-00eb-486e-82aa-8921ed0f1f6d"));
+        Assertions.assertEquals(actualUserResponse, Optional.of(expectedUserResponse));
     }
 
     @Test
